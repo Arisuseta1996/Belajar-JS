49eb3a1233cba083925ce91b3a71b0a7bc2e2dbc
Ari suseta <100476409+Arisuseta1996@users.noreply.github.com>
2022-03-03 22:04:53 +0700

    ojek1

--- forloop.js
+++ forloop.js
@@ -1,3 +1,8 @@
-for(var i = 1; i <=5; i++){
-    console.log('looping ke-'+i);
+// for(var i = 1; i <=5; i++){
+//     console.log('looping ke-'+i);
+// }
+var jmlOjek = 5;
+
+for(var noOjek = 1; noOjek <= jmlOjek; noOjek++){
+    console.log('Ojek No. '+ noOjek + ' sedang narik');
 }
