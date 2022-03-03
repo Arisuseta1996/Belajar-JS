c12914efc982b4dd7bf0b34fc1a215545f6f4ae4
Ari suseta <100476409+Arisuseta1996@users.noreply.github.com>
2022-03-03 22:15:27 +0700

    latihan for dan while

--- forloop.js
+++ forloop.js
@@ -1,8 +1,17 @@
 // for(var i = 1; i <=5; i++){
 //     console.log('looping ke-'+i);
 // }
-var jmlOjek = 5;
 
-for(var noOjek = 1; noOjek <= jmlOjek; noOjek++){
+// updet ojek tidak beroperasi 4 -5
+var jmlOjek = 5;
+var ojkBeroperasi = 3;
+var noOjek = 1;
+
+while (noOjek <= ojkBeroperasi){
     console.log('Ojek No. '+ noOjek + ' sedang narik');
+    noOjek++;
+}
+
+for(var noOjek = ojkBeroperasi +1; noOjek <= jmlOjek; noOjek++){
+    console.log('Ojek No. '+ noOjek + ' sedang tidak narik');
 }
