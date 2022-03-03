65737fa478e53a35c0610378f28602ba9b3b84b4
Ari suseta <100476409+Arisuseta1996@users.noreply.github.com>
2022-03-03 22:32:33 +0700

    latihan loop ojk1

--- whileLoop.js
+++ whileLoop.js
@@ -1,15 +1,30 @@
 // while loop
 
-var i = 1;
+// var i = 1;
 
-while(i <= 5){
-    console.log('Loop ke '+i);
-    // increment
-    i++;
+// while(i <= 5){
+//     console.log('Loop ke '+i);
+//     // increment
+//     i++;
+// }
+
+// // for loop
+
+// for(var x = 1; x <= 5; x++){
+//     console.log('looping ke '+ x +'x');
+// }
+
+// latihan buah
+
+var jmlOjk = 10;
+var ojkBeroperasi = 9;
+var iOjk = 1;
+// beroperasi
+while(iOjk <= ojkBeroperasi){
+    console.log('Ojek No. '+iOjk+' beroperasi');
+    iOjk++;
 }
-
-// for loop
-
-for(var x = 1; x <= 5; x++){
-    console.log('looping ke '+ x +'x');
+// belum ke bengkel
+for (iOjk = ojkBeroperasi + 1; iOjk <= jmlOjk; iOjk++) {
+    console.log('Ojek No. '+iOjk+' lagi rusak("belum ke bengkel")');
 }
